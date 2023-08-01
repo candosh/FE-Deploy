6554fad5658cb0b715a584ed9fb7c8185bcad1bc
최서현 <csh200310@naver.com>
2023-08-01 19:24:21 +0900

    init

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
                 </div>
                 <div className="card back">
                     <div>
-                        <span className="name">숭멋사</span>
+                        <span className="name">최서현</span>
                         <span className="position">FE</span>
                     </div>
-                    <a className="github"></a>
+                    <a className="github" href="https://github.com/candosh"></a>
                 </div>
             </div>
         </>
